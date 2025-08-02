ee724951edd1c12890947370f8e75c9e0dd8b4fa
Sahil Singh <imgohan444@gmail.com>
2025-08-02 12:36:29 +0530

    tuples

--- 16-tuplesBasics.py
+++ 16-tuplesBasics.py
@@ -20,3 +20,13 @@ if  342 in tup:
   print("Yes 342 is present in this tuple")
 tup2 = tup[1:4]
 print(tup2)
+
+#Operations in Tuples
+
+tuple1 = (0, 1, 2, 3, 2, 31, 1, 3, 2, 3)
+# res = tuple1.count(3)
+# res = tuple1.index(3)
+# res = tuple1.index(311)
+# res = tuple1.index(3, 4, 8)
+res = len(tuple1)
+print('Count of 3 in tuple1 is:', res)
